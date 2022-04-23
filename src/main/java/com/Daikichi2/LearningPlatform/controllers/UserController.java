ba05c2e9cd2c43667d1e2f7d3db6a3e4b5e1e6e2
Cheng Yu <yucheng618@gmail.com>
2022-04-22 22:09:32 -0400

    updated index/teacher register/student regist

---
+++ src/main/java/com/Daikichi2/LearningPlatform/controllers/UserController.java
@@ -0,0 +1,65 @@
+package com.Daikichi2.LearningPlatform.controllers;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.Daikichi2.LearningPlatform.models.User;
+import com.Daikichi2.LearningPlatform.services.UserService;
+
+@Controller
+public class UserController {
+	@Autowired
+	public UserService userService;
+
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
+	
+	@GetMapping("/consider")
+	public String consider_page(Model model) {
+		model.addAttribute("user", new User());
+		
+		return "consideration";
+	}
+	
+	@GetMapping("/teacher_new")
+	public String teacher_new(@ModelAttribute("teacher") User user) {
+		user.setRole("teacher");
+		return "teacherRegister";
+	}
+	
+	@PostMapping("/teacher_save")
+	public String teacher_save(@Valid @ModelAttribute("teacher") User user, BindingResult result) {
+		if(result.hasErrors()) {
+			return "teacherRegister";
+		}
+		User errors = userService.register(user, result);
+		if(errors == null) return "teacherRegister";
+		return "redirect:/courses";		
+	}
+	
+	@GetMapping("/student_new")
+	public String student_new(@ModelAttribute("student") User user) {
+		user.setRole("student");
+		return "studentRegister";
+	}
+	
+	@PostMapping("/student_save")
+	public String student_save(@Valid @ModelAttribute("student") User user, BindingResult result) {
+		if(result.hasErrors()) {
+			return "studentRegister";
+		}
+		User errors = userService.register(user, result);
+		if(errors == null) return "studentRegister";
+		return "redirect:/courses";		
+	}
+	
+}
