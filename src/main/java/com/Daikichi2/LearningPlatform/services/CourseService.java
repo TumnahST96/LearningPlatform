c6a4c969df87ef1e891326f9c615a75a7c6e9b4a
TumnahST96 <ts25583@email.vccs.edu>
2022-04-25 12:44:11 -0400

    repository and service

--- src/main/java/com/Daikichi2/LearningPlatform/services/CourseService.java
+++ src/main/java/com/Daikichi2/LearningPlatform/services/CourseService.java
@@ -1,8 +1,11 @@
 package com.Daikichi2.LearningPlatform.services;
 
 import java.util.List;
+import java.util.Optional;
 
+import com.Daikichi2.LearningPlatform.models.Course;
 import com.Daikichi2.LearningPlatform.repositories.CoursesRepository;
+
 
 
 public class CourseService {
@@ -18,6 +21,35 @@ public class CourseService {
 		public List<Course>allCourses(){
 			return courseRepo.findAll();
 		}
+		//create courses
 		
+		public Course createCourse(Course course) {
+			return courseRepo.save(course);
+		}
+		
+		//find one Course
+		public Course findCourse(Long id) {
+			
+			Optional<Course> optCourse = courseRepo.findById(id);
+			
+			if(optCourse.isPresent()) {
+				return optCourse.get();
+			}
+			else return null;
+		}
+		
+		
+		//delete a Course from rpeo
+		
+		public void CourseMan(Long id) {
+			
+			courseRepo.deleteById(id);
+		}
+		
+		
+		//update one Course
+		public Course updateCourse(Course course) {
+			return courseRepo.save(course);
+		}	
 
 };
