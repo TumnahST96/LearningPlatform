5cb65294b6d25ab209169e9b183a1bfeafff3b6d
Cheng Yu <yucheng618@gmail.com>
2022-04-25 13:05:37 -0400

    updated CourseController

--- src/main/java/com/Daikichi2/LearningPlatform/repositories/CoursesRepository.java
+++ src/main/java/com/Daikichi2/LearningPlatform/repositories/CoursesRepository.java
@@ -3,10 +3,11 @@ package com.Daikichi2.LearningPlatform.repositories;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.Daikichi2.LearningPlatform.models.Course;
 
-
+@Repository
 public interface CoursesRepository extends CrudRepository<Course, Long>{
 	
 	List<Course>findAll();
