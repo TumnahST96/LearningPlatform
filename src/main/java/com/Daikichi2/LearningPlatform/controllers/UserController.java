2547aa56b157750c6b49c5c9fe9f05a3adba1bf4
TumnahST96 <ts25583@email.vccs.edu>
2022-04-25 14:04:23 -0400

    single book shown

--- src/main/java/com/Daikichi2/LearningPlatform/controllers/UserController.java
+++ src/main/java/com/Daikichi2/LearningPlatform/controllers/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.Daikichi2.LearningPlatform.models.User;
@@ -74,6 +75,16 @@ public class UserController {
 		model.addAttribute("allCourses", courseServe.allCourses());
 		return "courses";
 	}
+	
+	
+	@GetMapping("/singleCourse/{id}")
+	public String SingleCourse (@PathVariable("id")Long id,  Model model) {
+	
+		model.addAttribute("course", courseServe.findCourse(id));
+		return "singleCourse";
+	}
+	
+	
 	
 	
 	//=======================================//
